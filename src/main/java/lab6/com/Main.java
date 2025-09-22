d5a5b91c71e2ad36e954bb744fbd68c035e6d17d
Paloma Gomez Abad <paloma.gomez@gruposantander.com>
2025-09-22 16:06:23 +0200

    date branch

--- src/main/java/lab6/com/Main.java
+++ src/main/java/lab6/com/Main.java
@@ -3,5 +3,7 @@ package lab6.com;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // print current date and time
+        System.out.println(java.time.LocalDateTime.now());
     }
 }
